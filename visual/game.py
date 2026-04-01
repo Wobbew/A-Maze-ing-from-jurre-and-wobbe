c602205733b1e8d7502358900f8014e6ab0113a0
Wobbe linux <Wobbew@gmail.com>
2026-04-01 18:01:59 +0200

    i made it work whit key W

--- visual/game.py
+++ visual/game.py
@@ -17,22 +17,15 @@ class player:
         self.facing = facing
         self.maze = maze
 
-    def find_view(self, max_depth=3):
-        if self.facing == "N":
-            return self.view_N(max_depth)
-
-    def view_N(self, max_depth):
+    def view(self, max_depth =3):
         view = []
 
         for depth in range(1, max_depth + 1):
-            y = self.Y - depth + 1
-            x = self.X
+            pos = cells_around(self, 0, 0, depth)
 
-            front = self.get_wall(y, x, 1)
-
-            right = self.get_wall(y, x, 2)
-
-            left = self.get_wall(y, x, 8)
+            front = self.get_wall(pos, facing_to_bit(self, "F"))
+            right = self.get_wall(pos, facing_to_bit(self, "R"))
+            left  = self.get_wall(pos, facing_to_bit(self, "L"))
 
             view.append((left, front, right))
 
@@ -41,27 +34,44 @@ class player:
 
         return view
 
-    def get_wall(self, y, x, mask):
-        if not self.is_valid(y, x):
+    def get_wall(self, pos, mask):
+        x, y =pos
+        if not self.is_valid((y, x)):
             return True 
         return bool(self.maze[y][x] & mask)
 
-    def is_valid(self, y, x):
+    def is_valid(self, pos):
+        x, y =pos
         if y < 0 or x < 0 or y >= len(self.maze) or x >= len(self.maze[0]):
             return False
         return True
 
+    def move(self, window):
+        x, y = cells_around(self, 0, 0, 1)
+        if self.is_valid((x, y)):
+            self.X = x
+            self.Y = y
+            mlx.mlx_clear_window(ptr, window)
+            tmp_name(self, window)
+            print(self.X, self.Y)
+        else:
+            print(test)
+
+            
+            
+
 
 def tmp_name(p, window):
-    view = p.find_view()
+    view = p.view()
+    mlx.mlx_put_image_to_window(ptr, window, get_image("images/floor and ceiling.png"), 0, 0)
     print(f"view: {view}\n p {p.maze[p.Y][p.X]}")  
     for depth in reversed(range(len(view))):
         left, front, right = view[depth]
         print(f"depth={depth} left={left} front={front} right={right}") 
-        if front:
-            draw_front(depth+1, window)
         draw_left_wall(p, depth+1, window, wall=left)
         draw_right_wall(p, depth+1, window, wall=right)
+        if front:
+            draw_front(depth+1, window)
 
 def draw_front(depth, window):
     if depth == 3:
@@ -86,7 +96,7 @@ def draw_left_wall(p, depth, window, max_depth = 3, wall = True):
     # if not wall:
     #     if depth < max_depth:
     #         if depth + 1 < max_depth:
-    #             if p.get_wall(p.Y + depth, p.X - 1, 1):
+    #             if p.get_wmlx.mlx_put_image_to_window(ptr, window, get_image("images/floor and ceiling.png"), 0, 0)all(p.Y + depth, p.X - 1, 1):
     #                 pass
     #             else:
     #                 if depth + 2 < max_depth:
@@ -102,29 +112,48 @@ def draw_right_wall(p, depth, window, max_depth=3, wall=True):
         if wall:
             mlx.mlx_put_image_to_window(ptr, window, get_image("images/RWall2.png"), 0, 0)
         else:
-            if p.get_wall(p.Y + depth, p.X - 1, 1):
+            if p.get_wall(cells_around(p, 0, 1, 1), facing_to_bit(p, "F")):
                 mlx.mlx_put_image_to_window(ptr, window, get_image("images/FWall+1_2.png"), 0, 0)
     if depth == 1:
         if wall:
             mlx.mlx_put_image_to_window(ptr, window, get_image("images/RWall1.png"), 0, 0)
         else:
-            if p.get_wall(p.Y + depth, p.X - 1, 1):
+            if p.get_wall(cells_around(p, 0, 1, 0), facing_to_bit(p, "F")):
                 mlx.mlx_put_image_to_window(ptr, window, get_image("images/FWall+1 1.png"), 0, 0)
-            elif p.get_wall(p.Y + depth + 1, p.X - 1, 4):
+            elif p.get_wall(cells_around(p, 0, 1, 1), facing_to_bit(p, "R")):
                     mlx.mlx_put_image_to_window(ptr, window, get_image("images/RWall+1_2.png"), 0, 0)
-
+def cells_around(p, to_L, to_R, to_F):
+    if p.facing == 'N':
+        return (p.X + to_R - to_L, p.Y + to_F)
+    if p.facing == 'S':
+        return (p.X + to_L - to_R, p.Y - to_F)
+    if p.facing == 'E':
+        return (p.X + to_F, p.Y + to_L - to_R)
+    if p.facing == 'W':
+        return (p.X - to_F, p.Y + to_R - to_L)
+    
+def facing_to_bit(p, side):
+    mapping = {
+        'N': {'F':1, 'R':2, 'B':4, 'L':8},
+        'E': {'F':2, 'R':4, 'B':8, 'L':1},
+        'S': {'F':4, 'R':8, 'B':1, 'L':2},
+        'W': {'F':8, 'R':1, 'B':2, 'L':4},
+    }
+    return mapping[p.facing][side]
+    
 
 def render_3d(maze, X=0, Y=0, facing="N"):
     p = player(Y, X, facing, maze)
 
     window = mlx.mlx_new_window(ptr, 1920, 1080, "test")
 
-    def on_key(keynum, dummy):
-        if keynum == KEY_ESCAPE:  
+    def on_key(keynum, param):
+        if keynum == KEY_ESCAPE:
             mlx.mlx_loop_exit(ptr)
+        if keynum == KEY_W:
+            p.move(window)
 
     mlx.mlx_key_hook(window, on_key, None)
-    mlx.mlx_put_image_to_window(ptr, window, get_image("images/floor and ceiling.png"), 0, 0)
     tmp_name(p, window)
     mlx.mlx_loop(ptr)
 
