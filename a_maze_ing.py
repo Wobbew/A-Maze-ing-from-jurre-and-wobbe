12671f2405fd9ffb6eb787c7ee1fbdf7f117b628
Jurre Stomps <jstomps@f1r6s19.codam.nl>
2026-05-06 14:23:37 +0200

    OMG it work

--- a_maze_ing.py
+++ a_maze_ing.py
@@ -7,12 +7,20 @@ class maze():
         self.height = int(dic.get("HEIGHT"))
         self.width = int(dic.get("WIDTH"))
         self.perfect = str(dic.get("PERFECT"))
-        self.entry = tuple(dic.get("ENTRY"))
-        self.exit = tuple(dic.get("EXIT"))
+        self.entry = tuple(int(v) for v in dic.get("ENTRY").split(","))
+        self.exit = tuple(int(v) for v in dic.get("EXIT").split(","))
         if dic.get("SEED") != "0":
-            self.seed = random.seed(dic.get("SEED"))
+            random.seed(dic.get("SEED"))
         else:
-            self.seed = random.seed()
+            random.seed()
+
+    def write_hex(self, filename="maze.txt"):
+        with open(filename, "w") as f:
+            for row in self.list_dict:
+                line = ""
+                for cell in row:
+                    line += format(cell["walls"], "X")
+                f.write(line + "\n")
 
     def maze_gen(self):
         total = self.width * self.height
@@ -24,18 +32,22 @@ class maze():
         self.y = y
         marked = 0
         last_multioption = []
-        multioption_count = 0
         while marked != total:
             if list_dict[y][x]["walls"] not in (1, 2, 4, 8):
                 last_multioption.append([x, y])
-                multioption_count += 1
             self.x = x
             self.y = y
+            print(x, y)
             choice = self.Random()
-            if choice == "Error":
+            while choice == "Error":
                 if last_multioption:
-                    x, y = last_multioption[-1]
-                choice = self.Random()
+                    # print(last_multioption)
+                    x, y = last_multioption.pop()
+                    self.x = x
+                    self.y = y
+                    choice = self.Random()
+                if choice == "Error":
+                    continue
             if choice == "N":
                 list_dict[y][x]["walls"] &= ~0b1000
                 list_dict[y - 1][x]["walls"] &= ~0b0100
@@ -52,27 +64,27 @@ class maze():
                 list_dict[y][x]["walls"] &= ~0b0001
                 list_dict[y][x - 1]["walls"] &= ~0b0010
                 x -= 1
-            else:
-                if last_multioption:
-                    [x, y] = last_multioption[multioption_count - 1]
-                multioption_count -= 1
             if not list_dict[y][x]["marked"]:
                 list_dict[y][x]["marked"] = True
                 marked += 1
+        print(marked)
+        return list_dict
 
     def logostamp(self):
-        placeble = False
+        placeable = False
         for y in range(self.height):
             for x in range(self.width):
-                placeble = self.hardlogo(x, y)
-                if placeble:
+                placeable = self.hardlogo(x, y)
+                if placeable:
                     break
-        if not placeble:
+            if placeable:
+                break
+        if not placeable:
             print("42error: No available space to place 42")
         else:
             for n in self.fortytwo:
                 xtmp, ytmp = n
-                self.list_dict[xtmp][ytmp]["marked"] = True
+                self.list_dict[ytmp][xtmp]["marked"] = True
 
     def hardlogo(self, x, y) -> bool:
         fortytwo = [[x - 3, y - 2],
@@ -94,11 +106,12 @@ class maze():
                     [x + 2, y + 2],
                     [x + 3, y + 2]]
         self.fortytwo = fortytwo
-        possible = True
+
         for loc in fortytwo:
-            if loc == self.entry or self.exit:
-                possible = False
-            if not possible:
+            lx, ly = loc
+            if not (0 <= lx < self.width and 0 <= ly < self.height):
+                return False
+            if list(loc) == list(self.entry) or list(loc) == list(self.exit):
                 return False
         return True
 
@@ -114,10 +127,9 @@ class maze():
             choice.append("N")
         if y < self.height - 1 and self.list_dict[y + 1][x]["marked"] is False:
             choice.append("S")
-        try:
-            choice = random.choice(choice)
-        except IndexError:
+        if choice == []:
             return "Error"
+        choice = random.choice(choice)
         return choice
 
 
@@ -125,14 +137,15 @@ if __name__ == "__main__":
     try:
         name = sys.argv
         dic = dict()
-        with open(name[1]) as file:
-            info = file.read()
+        with open(name[1]) as file1:
+            info = file1.read()
             tmp = info.split("\n")
             for i in tmp:
                 if '=' in i:
-                    key, inp = i.split('=')
+                    key, inp = i.split('=', 1)
                     dic.update({key: inp})
         m = maze(dic)
         m.maze_gen()
+        m.write_hex("maze.txt")
     except FileNotFoundError:
         print("File not found")
